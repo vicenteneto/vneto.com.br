dd5790fea08eef2309eba0f7c6aba184d624000c
Vicente Neto <vicente.silva@embedded.ufcg.edu.br>
2016-06-05 23:40:59 -0300

    Initial code generated by Yeoman Generator

---
+++ src/app/index.module.js
@@ -0,0 +1,8 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('vneto', ['ngAnimate', 'ngCookies', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial',
+      'toastr']);
+
+})();
